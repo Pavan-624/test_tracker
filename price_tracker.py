b4e354c25e3bbbc3f794e7cedfb9917276e812d8
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-19 19:05:20 +0530

    script changed

--- price_tracker.py
+++ price_tracker.py
@@ -2,10 +2,10 @@ import json
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-
-# Load environment variables from the ENV_DATA secret
 import os
 import ast
+
+# Load environment variables from ENV_DATA secret
 config = ast.literal_eval(os.getenv('ENV_DATA'))
 
 # Setup Firefox options
@@ -17,16 +17,16 @@ options = Options()
 service = Service(executable_path='/usr/local/bin/geckodriver')
 
 # Initialize the Firefox WebDriver
-driver = webdriver.Firefox(service=service, options=options)
+try:
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get("https://www.amazon.in/s?k=iphone+15")
 
-# Use the driver for your automation tasks
-driver.get("https://www.amazon.in/s?k=iphone+15")
+    # Example of using email settings from config
+    from_email = config["FROM_EMAIL"]
+    to_email = config["TO_EMAIL"]
+    email_password = config["EMAIL_PASSWORD"]
 
-# Example of using email settings from config
-from_email = config["FROM_EMAIL"]
-to_email = config["TO_EMAIL"]
-email_password = config["EMAIL_PASSWORD"]
+    # Your email sending code here
 
-# Your email sending code here
-
-driver.quit()
+finally:
+    driver.quit()
