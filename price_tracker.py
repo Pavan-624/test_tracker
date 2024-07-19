310a8d178743b53789fe837b8bd3fef7c23cf566
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-19 23:00:41 +0530

    script changed

--- price_tracker.py
+++ price_tracker.py
@@ -7,16 +7,10 @@ geckodriver_path = '/usr/local/bin/geckodriver'
 
 # Set up Firefox options
 options = Options()
-options.add_argument('--headless')  # Run Firefox in headless mode (no GUI)
-options.add_argument('--no-sandbox')  # Disable sandboxing to avoid security restrictions in CI environments
+# options.add_argument('--headless')  # Comment out or remove this line
 
 # Set up Firefox service
 service = Service(executable_path=geckodriver_path)
 
 # Initialize Firefox WebDriver
-try:
-    driver = webdriver.Firefox(service=service, options=options)
-    driver.get('https://www.amazon.in/Fossil-Analog-Black-Unisex-Watch/dp/B005LBZ6G6')
-    print("Page title is:", driver.title)
-finally:
-    driver.quit()
+driver = webdriver.Firefox(service=service, options=options)
