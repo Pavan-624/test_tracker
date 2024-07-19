cc22e46e8e359ed5ca2efbdb072a7192043a20a7
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-19 17:28:31 +0530

    script changed

--- price_tracker.py
+++ price_tracker.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 
-service = Service("C:/Users/Pavan/Downloads/chromedriver-win64 (1)/chromedriver-win64/chromedriver.exe")
+service = Service(r"C:\Users\Pavan\Downloads\chromedriver-win64 (1)\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://www.amazon.in/Fossil-Analog-Black-Unisex-Watch/dp/B005LBZ6G6")
