e9e0825ad6447551f4443b32a72b0b037c300f21
Pavan Thathikonda <pavanthatikonda235@gmail.com>
2024-07-19 10:43:30 +0530

    changed

--- price_tracker.py
+++ price_tracker.py
@@ -7,7 +7,3 @@ email_password = os.getenv('EMAIL_PASSWORD')
 to_email = os.getenv('TO_EMAIL')
 
 # Ensure these variables are being used correctly in your script
-print(f"From Email: {from_email}")
-print(f"To Email: {to_email}")
-
-# Your existing script logic here
